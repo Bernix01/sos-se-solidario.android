7dd7788d8ac47bc0fec9b0433c461a5af7a356da
whugo43 <whugo43@hotmail.com>
2016-08-14 11:38:05 -0500

    esta lo de login

--- app/src/main/java/paralelo1/poo/sossesolidario/MainActivity.java
+++ app/src/main/java/paralelo1/poo/sossesolidario/MainActivity.java
@@ -2,6 +2,7 @@ package paralelo1.poo.sossesolidario;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView nam;
+        nam = (TextView) findViewById(R.id.name);
+        nam.setText("ke vin");
     }
 }
