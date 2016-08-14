f10ed28267c1e3423d11554bd0a8ddfb0449e4dc
jvivas-rock <jvivas.trecool@gmail.com>
2016-08-14 17:29:24 -0500

    jvivas

---
+++ app/src/main/java/paralelo1/poo/sossesolidario/AndroidBasedeDatos.java
@@ -0,0 +1,43 @@
+package paralelo1.poo.sossesolidario;
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+/**
+ * Created by roche on 14/08/2016.
+ */
+public class AndroidBasedeDatos extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+
+        //Abrimos la base de datos 'DBUsuarios' en modo escritura
+        CASQLiteHelper usdbh =
+                new CASQLiteHelper(this, "DBUsuarios", null, 1);
+
+        NecesidadSQLiteHelper nsdbh =
+                new NecesidadSQLiteHelper(this, "DBUsuarios", null, 1);
+
+        SQLiteDatabase db = usdbh.getWritableDatabase();
+
+        //Si hemos abierto correctamente la base de datos
+        if(db != null)
+        {
+            //Insertamos 5 usuarios de ejemplo
+            for(int i=1; i<=5; i++)
+            {
+                //Generamos los datos
+                int codigo = i;
+                String nombre = "Usuario" + i;
+
+                //Insertamos los datos en la tabla Usuarios
+                db.execSQL("INSERT INTO Usuarios (codigo, nombre) " +
+                        "VALUES (" + codigo + ", '" + nombre +"')");
+            }
+
+            //Cerramos la base de datos
+            db.close();
+        }
+    }
+}
