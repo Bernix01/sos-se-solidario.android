7600818969e28dedddfc24108a294b96e08bb06f
whugo43 <whugo43@hotmail.com>
2016-08-14 13:24:14 -0500

    esta lo de login

---
+++ app/src/main/java/paralelo1/poo/sossesolidario/Usuario.java
@@ -0,0 +1,40 @@
+package paralelo1.poo.sossesolidario;
+
+/**
+ * Created by whugo on 14/8/2016.
+ */
+public class Usuario {
+    private String nombre;
+    private String correo;
+    private String contraseña;
+
+    public Usuario(String nombre, String correo, String contraseña) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
